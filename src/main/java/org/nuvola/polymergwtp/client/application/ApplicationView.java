17bfa74e968aa3c8a9c644ab3f73d987de64999c
imrabti <imrabti@gmail.com>
2015-12-06 07:34:38 +0000

    More small Changes.

--- src/main/java/org/nuvola/polymergwtp/client/application/ApplicationView.java
+++ src/main/java/org/nuvola/polymergwtp/client/application/ApplicationView.java
@@ -1,8 +1,10 @@
 package org.nuvola.polymergwtp.client.application;
 
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
+import com.vaadin.polymer.paper.widget.PaperDrawerPanel;
 
 import javax.inject.Inject;
 
@@ -10,6 +12,9 @@ import javax.inject.Inject;
 public class ApplicationView extends ViewImpl implements ApplicationPresenter.MyView {
     interface Binder extends UiBinder<Widget, ApplicationView> {
     }
+
+    @UiField
+    PaperDrawerPanel main;
 
     @Inject
     ApplicationView(Binder uiBinder) {
